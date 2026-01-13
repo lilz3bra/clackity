492f1d09481cb3075a57fb16f9b0f684abd848b9
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2026-01-13 01:22:24 -0300

    feat: refactor to increase readability. fix vim.o.timeoutlen issue in window.lua

--- lua/clackity/ui/init.lua
+++ lua/clackity/ui/init.lua
@@ -1,73 +1,25 @@
 local M = {}
 
-local create_window = function(opts)
-  opts = opts or {}
-
-  local buf = vim.api.nvim_create_buf(false, true)
-
-  local width = math.floor(vim.o.columns / 2)
-  local height = math.floor((vim.o.lines - 4) / 2)
-
-  local col = (vim.o.columns - width) / 2
-  local row = (vim.o.lines - height) / 2
-
-  local win_config = {
-    relative = "editor",
-    width = width,
-    height = height,
-    style = "minimal",
-    border = "rounded",
-    col = col,
-    row = row,
-  }
-  local win = vim.api.nvim_open_win(buf, true, win_config)
-  return { buf = buf, win = win }
-end
-local core = require("clackity.core")
-
-local original_cursor = ""
-local function set_fake_cursor()
-  original_cursor = vim.opt.guicursor:get()
-  vim.opt.guicursor = "n:ver25-blinkon0"
-end
-
-local function restore_cursor()
-  vim.opt.guicursor = original_cursor
-end
-
 M.start_window = function()
-  local float = create_window()
+  local window = require("clackity.ui.window")
+  local core = require("clackity.core")
+
+  local float = window.create_window()
   local words = core.get_words(4)
   local bufnr = float.buf
 
-  -- Fix for global keymaps being finicky
-  local augroup = vim.api.nvim_create_augroup("ClackityInputFix", { clear = true })
-  local original_timeout = vim.o.timeoutlen
+  window.buffer_keys_fix(bufnr)
 
-  vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
-    buffer = bufnr,
-    group = augroup,
-    callback = function()
-      original_timeout = vim.o.timeoutlen
-      vim.o.timeoutlen = 0
-    end,
-  })
-
-  vim.api.nvim_create_autocmd({ "BufLeave", "WinLeave" }, {
-    buffer = bufnr,
-    group = augroup,
-    callback = function()
-      vim.o.timeoutlen = original_timeout
-    end,
-  })
-  -- Rest of the ui logic
   core.bind_keys(float)
+
   vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, words)
-  set_fake_cursor()
+  window.set_fake_cursor()
+
   vim.api.nvim_set_option_value("virtualedit", "all", { scope = "local", win = float.win })
+  print(vim.o.timeoutlen, vim.o.ttimeoutlen)
   vim.keymap.set("n", "<C-q>", function()
     vim.api.nvim_win_close(float.win, true)
-    restore_cursor()
+    window.restore_cursor()
   end, { silent = true, buffer = bufnr })
 end
 
---
+++ lua/clackity/ui/window.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+M.create_window = function(opts)
+  opts = opts or {}
+
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  local width = math.floor(vim.o.columns / 2)
+  local height = math.floor((vim.o.lines - 4) / 2)
+
+  local col = (vim.o.columns - width) / 2
+  local row = (vim.o.lines - height) / 2
+
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    style = "minimal",
+    border = "rounded",
+    col = col,
+    row = row,
+  }
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+  return { buf = buf, win = win }
+end
+
+M.buffer_keys_fix = function(bufnr)
+  local augroup = vim.api.nvim_create_augroup("ClackityInputFix", { clear = true })
+  local original_timeout = vim.o.timeoutlen
+  local original_ttimeout = vim.o.ttimeoutlen
+
+  vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+    buffer = bufnr,
+    group = augroup,
+    callback = function()
+      original_timeout = vim.o.timeoutlen
+      original_ttimeout = vim.o.ttimeoutlen
+      vim.o.ttimeoutlen = 0
+      vim.o.timeoutlen = 0
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "BufLeave", "WinLeave" }, {
+    buffer = bufnr,
+    group = augroup,
+    callback = function()
+      vim.o.timeoutlen = original_timeout
+      vim.o.ttimeoutlen = original_ttimeout
+    end,
+  })
+
+  vim.o.ttimeoutlen = 0
+  vim.o.timeoutlen = 0
+end
+
+local original_cursor = ""
+
+M.restore_cursor = function()
+  vim.opt.guicursor = original_cursor
+end
+
+M.set_fake_cursor = function()
+  original_cursor = vim.opt.guicursor:get()
+  vim.opt.guicursor = "n:ver25-blinkon0"
+  return original_cursor
+end
+
+return M
