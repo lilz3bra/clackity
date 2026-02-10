46968bb9850f9eb5a0fab7e0d411836460283a93
Max Ojeda <21014124+lilz3bra@users.noreply.github.com>
2026-02-10 19:05:11 -0300

    feat: start adding type definitions

---
+++ lua/clackity/core/definitions.lua
@@ -0,0 +1,14 @@
+--- @alias KeyStatus "correct"|"error"
+
+--- @class Clackity.lesson_log
+--- @field target string
+--- @field actual string
+--- @field latency number
+--- @field status KeyStatus
+
+--- @class Clackity.stats.lesson
+--- @field total_chars number
+--- @field errors number
+--- @field wpm number
+--- @field accuracy number
+--- @field keys table
