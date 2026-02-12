5af627538f7ed90a4633b4562e54fec14c7f7e4e
Max Ojeda <21014124+lilz3bra@users.noreply.github.com>
2026-02-12 18:29:33 -0300

    feat: added sqlite.lua check

--- lua/clackity/init.lua
+++ lua/clackity/init.lua
@@ -3,6 +3,13 @@ local M = {}
 local config = require("clackity.core.config")
 
 function M.setup(opts)
+  local has_sqlite, sqlite = pcall(require, "sqlite")
+  if not has_sqlite then
+    vim.notify("Clackity: sqlite.lua is required but not found. Please check :checkhealth",
+      vim.log.levels.Error)
+    return
+  end
+
   config.setup(opts)
 end
 
