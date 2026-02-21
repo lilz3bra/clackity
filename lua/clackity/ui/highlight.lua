7f464f81d466035fac454172e2cc97bc59b59264
Max Ojeda <21014124+lilz3bra@users.noreply.github.com>
2026-02-21 00:44:26 -0300

    feat: added selection highlight

--- lua/clackity/ui/highlight.lua
+++ lua/clackity/ui/highlight.lua
@@ -6,6 +6,7 @@ local default_colors = {
   future = { link = "Comment", default = true },
   correct = { link = "Normal", default = true },
   error = { link = "Error", default = true },
+  selection = { link = "CursorLine", default = true },
 }
 
 function M.setup(opts)
@@ -17,12 +18,21 @@ function M.setup(opts)
   vim.api.nvim_set_hl(0, "ClackityCorrect", correct_opts)
   local error_opts = opts.error or default_colors.error
   vim.api.nvim_set_hl(0, "ClackityError", error_opts)
+  local sel_opts = opts.selection or default_colors.selection
+  vim.api.nvim_set_hl(0, "ClackitySelection", sel_opts)
 end
 
 function M.paint(buf, group, line, col_start, col_end)
   vim.hl.range(buf, M.ns_id, group, { line, col_start }, { line, col_end }, {
     priority = 200,
     strict = false,
+  })
+end
+
+function M.paint_line(buf, group, line_idx)
+  vim.api.nvim_buf_set_extmark(buf, M.ns_id, line_idx, 0, {
+    line_hl_group = group,
+    priority = 200,
   })
 end
 
