a1d393e263d8284e0bdf417f9c00a99c9ee9c3dd
Max Ojeda <21014124+lilz3bra@users.noreply.github.com>
2026-02-25 19:28:00 -0300

    added luacov to see test coverage

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,4 +1,24 @@
-local root = vim.fn.fnamemodify("./.tests", ":p")
+local luarocks_path = vim.fn.expand("~/.luarocks/share/lua/5.1/?.lua;") ..
+    vim.fn.expand("~/.luarocks/share/lua/5.1/?/init.lua;")
+local luarocks_cpath = vim.fn.expand("~/.luarocks/lib/lua/5.1/?.so;")
+
+package.path = package.path .. ";" .. luarocks_path
+package.cpath = package.cpath .. ";" .. luarocks_cpath
+
+local has_luacov, runner = pcall(require, "luacov.runner")
+if has_luacov then
+  runner.init()
+  -- Plenary exits abruptly. This forces LuaCov to write the stats file first.
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function()
+      runner.shutdown()
+    end
+  })
+else
+  print("\n[Clackity] WARNING: luacov not found. Coverage will not be generated.\n")
+end
+
+local root = vim.fn.fnamemodify("./.tests/", ":p")
 
 local function load_plugin(repo, name)
   local dir = root .. name
